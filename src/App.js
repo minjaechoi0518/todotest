51fb7dcfaeef24bc4296b0074128dcc77831dd8f
minjaechoi0518 <cmk7892@gmail.com>
2023-04-20 11:34:40 +0900

    todotest

--- src/App.js
+++ src/App.js
@@ -1,25 +1,58 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
 
-function App() {
+const App = () => {
+  const [content, setContent] = useState([]);
+  const [title, setTitle] = useState("");
+
+  const contentChangeHandler = (e) => {
+    if (e.target.name === "title") {
+      setTitle(e.target.value);
+    }
+  };
+
+  const buttonForAdd = () => {
+    if (title.length > 0) {
+      const newContent = {
+        id: content.length + 1,
+        title: title,
+      };
+      setContent([...content, newContent]);
+      setTitle("");
+    }
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="mainContainer">
+        <input
+          name="title"
+          onChange={contentChangeHandler}
+          className="titleBox"
+          value={title}
+        ></input>
+
+        <button onClick={buttonForAdd} className="buttonForAdd btnFirst">
+          추가하기
+        </button>
+      </div>
+      <div>
+        <div className="head clearfix">
+          <h1>Todo List</h1>
+        </div>
+      </div>
+      <div className="con_list">
+        {content.map((item) => {
+          return (
+            <div className="con_todo" key={item.id}>
+              <div className="title">{item.title}</div>
+              {/* <p>{item.todo}</p> */}
+            </div>
+          );
+        })}
+      </div>
     </div>
   );
-}
+};
 
 export default App;
